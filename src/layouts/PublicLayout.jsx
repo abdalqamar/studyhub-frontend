5a72fb04585ad0a35e3e6931f3acacc1f9654575
Abdal Qamar <abdalqamar10@gmail.com>
2026-01-12 13:12:05 +0530

    update footer

--- src/layouts/PublicLayout.jsx
+++ src/layouts/PublicLayout.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/common/Navbar";
 import Footer from "../components/common/Footer";
-import ScrollToTop from "../components/ScrolltoTop";
+import ScrollToTop from "../components/ScrollToTop";
 
 const PublicLayout = () => {
   return (
