ec27f3c3b23f111247f8d84657a4cc5c5930fc98
Abdal Qamar <abdalqamar10@gmail.com>
2026-03-11 15:19:13 +0530

    update axiox file

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -5,6 +5,7 @@ import { queryClient } from "../main.jsx";
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: true,
+  timeout: 5000,
 });
 
 export default axiosInstance;
